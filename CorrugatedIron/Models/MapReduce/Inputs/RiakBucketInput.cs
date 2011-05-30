422d13bfa3d335c13355079a0c848599510062e3
OJ Reeves <oj@buffered.io>
2011-05-30 22:34:10 +1000

    Toying with Map/Reduce structure ideas
    * Took some of the ideas from JP's stuff and changed them slightly
      to see what they'd look like with a more fluent interface.
    * Mucked with the object models a little to see what might work and
      what sucks.
    * Put some tests together to make sure I wasn't going barking mad
      and keeping things inline with the results that JP was getting.
    
    The interface will remain "string" based until past v0.1, so I think
    this is a valid approach. Down the track I'd like to actually compile
    the map/red queries from expressions. That'll have to come way down
    the track though, as there's lots of work involved there.

---
+++ CorrugatedIron/Models/MapReduce/Inputs/RiakBucketInput.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace CorrugatedIron.Models.MapReduce.Inputs
+{
+    public class RiakBucketInput : IRiakPhaseInput
+    {
+        private readonly string _bucket;
+
+        public RiakBucketInput(string bucket)
+        {
+            _bucket = bucket;
+        }
+
+        public virtual JsonWriter WriteJson(JsonWriter writer)
+        {
+            writer.WriteValue(_bucket);
+            return writer;
+        }
+
+        public static implicit operator RiakBucketInput(string bucket)
+        {
+            return new RiakBucketInput(bucket);
+        }
+    }
+}
---
+++ CorrugatedIron/Models/MapReduce/Inputs/RiakPhaseInputs.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CorrugatedIron.Models.MapReduce.Inputs
+{
+    public class RiakPhaseInputs
+    {
+        private readonly List<RiakBucketKeyInput> _bucketKeyInputs;
+        private readonly List<RiakBucketKeyArgInput> _bucketKeyArgInputs;
+        private readonly RiakBucketInput _bucketInput;
+
+        public RiakPhaseInputs(RiakBucketInput bucketInput)
+        {
+            _bucketInput = bucketInput;
+        }
+
+        public RiakPhaseInputs(List<RiakBucketKeyInput> bucketKeyInputs)
+        {
+            _bucketKeyInputs = bucketKeyInputs;
+        }
+
+        public RiakPhaseInputs(List<RiakBucketKeyArgInput> bucketKeyArgInputs)
+        {
+            _bucketKeyArgInputs = bucketKeyArgInputs;
+        }
+
+        public JsonWriter WriteJson(JsonWriter writer)
+        {
+            if (_bucketInput != null)
+            {
+                return _bucketInput.WriteJson(writer);
+            }
+
+            writer.WriteStartArray();
+            if (_bucketKeyInputs != null)
+            {
+                _bucketKeyInputs.ForEach(i =>
+                    {
+                        writer.WriteStartArray();
+                        i.WriteJson(writer);
+                        writer.WriteEndArray();
+                    });
+            }
+            else
+            {
+                _bucketKeyArgInputs.ForEach(i =>
+                    {
+                        writer.WriteStartArray();
+                        i.WriteJson(writer);
+                        writer.WriteEndArray();
+                    });
+            }
+            writer.WriteEndArray();
+
+            return writer;
+        }
+
+        public static implicit operator RiakPhaseInputs(RiakBucketInput input)
+        {
+            return new RiakPhaseInputs(input);
+        }
+
+        public static implicit operator RiakPhaseInputs(List<RiakBucketKeyInput> input)
+        {
+            return new RiakPhaseInputs(input);
+        }
+
+        public static implicit operator RiakPhaseInputs(List<RiakBucketKeyArgInput> input)
+        {
+            return new RiakPhaseInputs(input);
+        }
+    }
+}
