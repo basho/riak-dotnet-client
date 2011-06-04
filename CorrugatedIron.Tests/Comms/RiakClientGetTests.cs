74d5b016fd9a2357bad1f40fd728a69d59806724
OJ Reeves <oj@buffered.io>
2011-06-04 14:11:05 +1000

    Toying with test structures.
    Former-commit-id: 547f6e35a907da3be00c9ef8dc809d5a15f1f5a7

---
+++ CorrugatedIron.Tests/Comms/RiakClientGetTests.cs
@@ -0,0 +1,46 @@
+﻿using CorrugatedIron.Comms;
+using CorrugatedIron.Messages;
+using CorrugatedIron.Models;
+using NUnit.Framework;
+
+namespace CorrugatedIron.Tests.Comms.RiakClientGetTests
+{
+    public abstract class RiakClientGetTestBase : RiakClientTestBase<RpbGetResp>
+    {
+        protected RiakResult<RiakObject> Result;
+        protected string Bucket = "test-bucket";
+        protected string Key = "test-key";
+        protected uint? Rval;
+
+        protected RiakClientGetTestBase(RiakResult<RpbGetResp> result)
+            : base(result)
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Result = Rval.HasValue ? Client.Get(Bucket, Key, Rval.Value) : Client.Get(Bucket, Key);
+        }
+    }
+
+    public class WhenGettingObjectFromRiak : RiakClientGetTestBase
+    {
+        public WhenGettingObjectFromRiak()
+            : base(RiakResult<RpbGetResp>.Success(new RpbGetResp
+            {
+                VectorClock = System.Text.Encoding.Default.GetBytes("somerandom"),
+                Content = new List<RpbContent>
+                {
+                }
+            }))
+        {
+        }
+
+        [Test]
+        public void RvalIsSetToTheProperDefault()
+        {
+            
+        }
+    }
+}
---
+++ CorrugatedIron.Tests/Comms/RiakClientPingTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CorrugatedIron.Comms;
+using CorrugatedIron.Tests.Extensions;
+using CorrugatedIron.Messages;
+using Moq;
+using NUnit.Framework;
+
+namespace CorrugatedIron.Tests.Comms.RiakClientTests
+{
+    public abstract class RiakClientPingTestBase : RiakClientTestBase<RpbPingResp>
+    {
+        protected RiakResult Result;
+
+        protected RiakClientPingTestBase(RiakResult<RpbPingResp> response)
+            : base(response)
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Result = Client.Ping();
+        }
+
+        [Test]
+        public void PbcClientIsInvoked()
+        {
+            Cluster.Verify(m => m.UseConnection(It.IsAny<byte[]>(), It.IsAny<Func<IRiakConnection, RiakResult<RpbPingResp>>>()), Times.Once());
+        }
+    }
+
+    [TestFixture]
+    public class WhenCallingPingWithError : RiakClientPingTestBase
+    {
+        public WhenCallingPingWithError()
+            : base(RiakResult<RpbPingResp>.Error(ResultCode.CommunicationError))
+        {
+        }
+
+        [Test]
+        public void SuccessResultIsReturned()
+        {
+            Result.IsSuccess.ShouldBeFalse();
+            Result.ResultCode.ShouldEqual(ResultCode.CommunicationError);
+        }
+    }
+
+    [TestFixture]
+    public class WhenCallingPingSuccessfully : RiakClientPingTestBase
+    {
+        public WhenCallingPingSuccessfully()
+            : base(RiakResult<RpbPingResp>.Success(new RpbPingResp()))
+        {
+        }
+
+        [Test]
+        public void SuccessResultIsReturned()
+        {
+            Result.IsSuccess.ShouldBeTrue();
+        }
+    }
+}
