547f6e35a907da3be00c9ef8dc809d5a15f1f5a7
OJ Reeves <oj@buffered.io>
2011-06-04 14:11:05 +1000

    Toying with test structures.

---
+++ CorrugatedIron.Tests/Comms/RiakClientTestBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CorrugatedIron.Comms;
+using Moq;
+
+namespace CorrugatedIron.Tests.Comms
+{
+    public abstract class RiakClientTestBase
+    {
+        protected Mock<IRiakCluster> Cluster;
+        protected Mock<IRiakConnection> Conn;
+        protected RiakClient Client;
+
+        protected RiakClientTestBase(RiakResult result, byte[] clientId = null)
+        {
+            Conn = new Mock<IRiakConnection>();
+            Cluster = new Mock<IRiakCluster>();
+
+            if(clientId != null)
+            {
+                Cluster.Setup(m => m.UseConnection(clientId, It.IsAny<Func<IRiakConnection, RiakResult>>())).Returns(result);
+                Client = new RiakClient(Cluster.Object, clientId);
+            }
+            else
+            {
+                Cluster.Setup(m => m.UseConnection(It.IsAny<byte[]>(), It.IsAny<Func<IRiakConnection, RiakResult>>())).Returns(result);
+                Client = new RiakClient(Cluster.Object);
+            }
+        }
+    }
+
+    public abstract class RiakClientTestBase<TResult>
+    {
+        protected Mock<IRiakCluster> Cluster;
+        protected Mock<IRiakConnection> Conn;
+        protected RiakClient Client;
+
+        protected RiakClientTestBase(RiakResult<TResult> result, byte[] clientId = null)
+        {
+            Conn = new Mock<IRiakConnection>();
+            Cluster = new Mock<IRiakCluster>();
+
+            if(clientId != null)
+            {
+                Cluster.Setup(m => m.UseConnection(clientId, It.IsAny<Func<IRiakConnection, RiakResult<TResult>>>())).Returns(result);
+                Client = new RiakClient(Cluster.Object, clientId);
+            }
+            else
+            {
+                Cluster.Setup(m => m.UseConnection(It.IsAny<byte[]>(), It.IsAny<Func<IRiakConnection, RiakResult<TResult>>>())).Returns(result);
+                Client = new RiakClient(Cluster.Object);
+            }
+        }
+    }
+}
