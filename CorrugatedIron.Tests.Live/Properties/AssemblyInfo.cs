ba6af1c6a240b23e896a3d863e548f39194771ac
OJ Reeves <oj@buffered.io>
2011-06-06 21:13:44 +1000

    fix #10 -- Handled versioning of the assemblies
    * Removed evidence of common assembly information from each of the
      projects and moved it to one central location in Metadata/VersionInfo.cs
      This makes it easy to update the version number for all of the
      components at once.
    * Each of the projects link to this new VersionInfo.cs file (the link
      is in the "Properties" folder) so that they all get the same version.
    * When we do releases, we just have to update the version number in
      VersionInfo.cs and all the files will pick up the new version.
    
    
    Former-commit-id: c852eac9644730e06b80f201dfba4e30c660c109

--- CorrugatedIron.Tests.Live/Properties/AssemblyInfo.cs
+++ CorrugatedIron.Tests.Live/Properties/AssemblyInfo.cs
@@ -1,36 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("CorrugatedIron.Tests.Live")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CorrugatedIron.Tests.Live")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("189a7e56-3cc2-4aa1-b4c0-99a5f6ec197d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Collection of live tests for the CorrugatedIron library.")]
---
+++ Metadata/VersionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CorrugatedIron")]
+[assembly: AssemblyProduct("CorrugatedIron")]
+[assembly: AssemblyCopyright("Copyright © OJ Reeves & Jeremiah Peschka 2011")]
+[assembly: AssemblyTrademark("CorrugatedIron")]
+[assembly: AssemblyCulture("")]
+
+// version: Major.Minor.Revision.Build
+// We'll use the minor version number for releases for now. When we get to a point
+// where we're really happy with the stability and features of the client we'll
+// move to the Major version number.
+// We'll use the revision number for hot fix release and probably leave the build
+// number alone. It's left here because Windows binaries use 4 numbers.
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
