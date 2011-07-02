2bbff8af2702740e43d07350e8ee491180300494
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2011-07-02 11:19:06 -0700

    * Added RiakObject.ToRiakObjectId() * Added RiakObjectTests.cs * Added test for RiakObject.ToRiakObjectId()

---
+++ CorrugatedIron.Tests/Models/RiakObjectTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CorrugatedIron.Models;
+using CorrugatedIron.Tests.Extensions;
+using NUnit.Framework;
+
+namespace CorrugatedIron.Tests.Models
+{
+    [TestFixture]
+    public class RiakObjectTests
+    {
+        private const string Bucket = "bucket";
+        private const string Key = "key";
+
+        [Test]
+        public void ToRiakObjectIdProducesAValidRiakObjectId()
+        {
+            var riakObject = new RiakObject(Bucket, Key, "value");
+            var riakObjectId = riakObject.ToRiakObjectId();
+
+            riakObjectId.Bucket.ShouldEqual(Bucket);
+            riakObjectId.Key.ShouldEqual(Key);
+        }
+    }
+}
