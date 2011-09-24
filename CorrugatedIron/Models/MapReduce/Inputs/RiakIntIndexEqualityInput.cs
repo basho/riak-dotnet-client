0a7adb849936cba55bdb065acd85d670df665faf
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2011-09-23 17:48:43 -0700

    Added new index querying inputs
    Former-commit-id: 23fa3c7a1356968ebecb9075e1f7331333b2a1a9

---
+++ CorrugatedIron/Models/MapReduce/Inputs/RiakIntIndexEqualityInput.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2010 - OJ Reeves & Jeremiah Peschka
+// 
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+using System;using Newtonsoft.Json;
+namespace CorrugatedIron.Models.MapReduce.Inputs
+{
+    public class RiakIntIndexEqualityInput : RiakPhaseInput
+    {        public string Bucket { get; set; }        public string Index { get; set; }        public int Key { get; set; }        
+        public RiakIntIndexEqualityInput (string bucket, string index, int key)
+        {            Bucket = bucket;            Index = index;            Key = key;
+        }                public override JsonWriter WriteJson(JsonWriter writer)        {            writer.WritePropertyName("inputs");                        writer.WriteStartObject();            writer.WritePropertyName("bucket");            writer.WriteValue(Bucket);                        writer.WritePropertyName("index");            writer.WriteValue(Index);                        writer.WritePropertyName("key");            writer.WriteValue(Key.ToString());            writer.WriteEndObject();                        return writer;        }
+    }
+}
+
