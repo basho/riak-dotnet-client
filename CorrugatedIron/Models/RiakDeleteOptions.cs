7b97c8a3aa83f0104781902278dbafd02061fafa
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2011-10-06 16:16:08 -0700

    * CorrugatedIron/Models/RiakDeleteOptions.cs Added XML doc.
    * CorrugatedIron/Util/RiakConstants.cs
    Added constants for quorum, one, default, and all
    
    
    Former-commit-id: 84efd648e7565dbf718c774a6c9f285aee2f0206

--- CorrugatedIron/Models/RiakDeleteOptions.cs
+++ CorrugatedIron/Models/RiakDeleteOptions.cs
@@ -16,17 +16,70 @@
 
 using CorrugatedIron.Messages;
 using CorrugatedIron.Util;
+using CorrugatedIron.Containers;
 
 namespace CorrugatedIron
 {
     public class RiakDeleteOptions
     {
+        /// <summary>
+        /// The number of replicas that need to agree when retrieving the object.
+        /// </summary>
+        /// <value>The RW Value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.</value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? Rw { get; set; }
+        
+        /// <summary>
+        /// The Vclock of the version that is being deleted. Use this to prevent deleting objects that have been modified since the last get request.
+        /// </summary>
+        /// <value>
+        /// The vclock.
+        /// </value>
+        /// <remarks>Review the information at http://wiki.basho.com/Vector-Clocks.html for additional information on how vector clocks 
+        /// are used in Riak.</remarks>
         public byte[] Vclock { get; set; }
+        
+        /// <summary>
+        /// The number of replicas that must return before a delete is considered a succes.
+        /// </summary>
+        /// <value>
+        /// The R value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? R { get; set; }
+        
+        /// <summary>
+        /// The number of replicas that must respond before a write is considered a success.
+        /// </summary>
+        /// <value>The W value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.</value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? W { get; set; }
+        
+        /// <summary>
+        /// Primary Read Quorum - the number of replicas that need to be available when retrieving the object.
+        /// </summary>
+        /// <value>
+        /// The primary read quorum. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? Pr { get; set; }
+        
+        /// <summary>
+        /// Primary Write Quorum - the number of replicas need to be available when the write is attempted.
+        /// </summary>
+        /// <value>
+        /// The primary write quorum. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? Pw { get; set; }
+        
+        /// <summary>
+        /// Durable writes - the number of replicas that must commit to durable storage before returning a successful response.
+        /// </summary>
+        /// <value>
+        /// The durable write value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
         public uint? Dw { get; set; }                public RiakDeleteOptions() {            Rw = RiakConstants.Defaults.RVal;        }
         
         internal void Populate(RpbDelReq request)
