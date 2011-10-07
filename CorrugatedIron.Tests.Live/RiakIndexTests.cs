2d318759e1e0e9876b8297820c4427d45d38fe84
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2011-10-06 17:33:10 -0700

    Merge from v0.2.0 develop
    Former-commit-id: ecc38ed61ef4835585074d60ac387ac6823f5821

---
+++ CorrugatedIron.Tests.Live/RiakIndexTests.cs
@@ -0,0 +1,176 @@
+// Copyright (c) 2010 - OJ Reeves & Jeremiah Peschka
+// 
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System.Linq;
+using System.Collections.Generic;
+using CorrugatedIron.Comms;
+using CorrugatedIron.Util;
+using CorrugatedIron.Models;
+using CorrugatedIron.Models.MapReduce;
+using CorrugatedIron.Models.MapReduce.Inputs;
+using CorrugatedIron.Tests.Extensions;
+using CorrugatedIron.Extensions;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace CorrugatedIron.Tests.Live
+{
+    [TestFixture()]
+    public class WhenUsingIndexes : RiakMapReduceTests
+    {
+        private const string _ageIndexName = "age";
+        
+        public WhenUsingIndexes () : base()
+        {
+            bucket = "riak_index_tests";
+        }
+        
+        [SetUp()]
+        public void SetUp()
+        {
+            Cluster = new RiakCluster(ClusterConfig, new RiakConnectionFactory());
+            ClientGenerator = () => new RiakClient(Cluster);
+            Client = ClientGenerator();
+        }
+        
+        [TearDown()]
+        public void TearDown()
+        {
+            Client.DeleteBucket(bucket);
+        }
+        
+        [Test()]
+        public void IndexesAreSavedWithAnObject()
+        {
+            var o = new RiakObject(bucket, "the_object", "{ value: \"this is an object\" }");
+            o.AddBinIndex("tacos", "are great!");
+            o.AddIntIndex("age", 12);
+            
+            Client.Put(o);
+            
+            var result = Client.Get(o.ToRiakObjectId());
+            
+            result.IsSuccess.ShouldBeTrue();
+            var ro = result.Value;
+            
+            ro.BinIndexes.Count.ShouldEqual(1);
+            ro.IntIndexes.Count.ShouldEqual(1);
+        }
+        
+        [Test()]
+        public void QueryingByIntIndexReturnsAListOfKeys()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var o = new RiakObject(bucket, i.ToString(), "{ value: \"this is an object\" }");
+                o.AddIntIndex("age_int", 32);
+                
+                Client.Put(o);
+            }
+            
+            var input = new RiakIntIndexEqualityInput(bucket, "age_int", 32);
+            
+            var mr = new RiakMapReduceQuery();
+            mr.Inputs(input)
+                .ReduceJs(r => r.Name("mapValuesJson").Keep(true));
+            
+            var result = Client.MapReduce(mr);
+            result.IsSuccess.ShouldBeTrue();
+            
+            var keys = result.Value.PhaseResults.OrderBy(pr => pr.Phase).ElementAt(0).GetObjects<RiakObjectId>();
+            
+            keys.Count().ShouldEqual(10);
+            
+            foreach (var key in keys)
+            {
+                key.Bucket.ShouldNotBeNullOrEmpty();
+                key.Key.ShouldNotBeNullOrEmpty();
+            }
+        }
+        
+        [Test()]
+        public void RiakObjectIdCanBeCreatedFromJsonArrayOrObject()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var o = new RiakObject(bucket, i.ToString(), "{ value: \"this is an object\" }");
+                o.AddIntIndex("age_int", 32);
+                
+                Client.Put(o);
+            }
+            
+            var input = new RiakIntIndexEqualityInput(bucket, "age_int", 32);
+            
+            var mr = new RiakMapReduceQuery();
+            mr.Inputs(input)
+                .ReduceJs(r => r.Name("mapValuesJson").Keep(true));
+            
+            var result = Client.MapReduce(mr);
+            result.IsSuccess.ShouldBeTrue();
+            var keysOne = result.Value.PhaseResults.OrderBy(pr => pr.Phase).ElementAt(0).GetObjects<RiakObjectId>();
+            
+            mr = new RiakMapReduceQuery();
+            mr.Inputs(input)
+                .ReduceErlang(r => r.ModFun("riak_kv_mapreduce", "reduce_identity").Keep(true));
+            
+            result = Client.MapReduce(mr);
+            result.IsSuccess.ShouldBeTrue();
+            var keysTwo = result.Value.PhaseResults.OrderBy(pr => pr.Phase).ElementAt(0).GetObjects<RiakObjectId>();
+            
+            keysOne.Count().ShouldEqual(keysTwo.Count());
+            
+            foreach (var key in keysOne)
+            {
+                key.Key.ShouldNotBeNullOrEmpty();
+                key.Bucket.ShouldNotBeNullOrEmpty();
+                
+                keysTwo.Contains(key).ShouldBeTrue();
+            }
+            
+            foreach (var key in keysTwo)
+            {
+                key.Key.ShouldNotBeNullOrEmpty();
+                key.Bucket.ShouldNotBeNullOrEmpty();
+                
+                keysOne.Contains(key).ShouldBeTrue();
+            }
+        }
+        
+        [Test()]
+        public void IntRangeQueriesReturnMultipleKeys()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var o = new RiakObject(bucket, i.ToString(), "{ value: \"this is an object\" }");
+                o.AddIntIndex("age_int", 25 + i);
+                
+                Client.Put(o);
+            }
+            
+            var input = new RiakIntIndexRangeInput(bucket, "age_int", 27, 30);
+            
+            var mr = new RiakMapReduceQuery()
+                .Inputs(input)
+                .ReduceErlang(r => r.ModFun("riak_kv_mapreduce", "reduce_identity").Keep(true));
+            
+            var result = Client.MapReduce(mr);
+            result.IsSuccess.ShouldBeTrue();
+            
+            // TODO write tests verifying results
+        }
+    }
+}
+
---
+++ CorrugatedIron/Models/RiakDeleteOptions.cs
@@ -0,0 +1,124 @@
+// Copyright (c) 2010 - OJ Reeves & Jeremiah Peschka
+// 
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using CorrugatedIron.Messages;
+using CorrugatedIron.Util;
+using CorrugatedIron.Containers;
+
+namespace CorrugatedIron
+{
+    public class RiakDeleteOptions
+    {
+        /// <summary>
+        /// The number of replicas that need to agree when retrieving the object.
+        /// </summary>
+        /// <value>The RW Value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.</value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? Rw { get; set; }
+        
+        /// <summary>
+        /// The Vclock of the version that is being deleted. Use this to prevent deleting objects that have been modified since the last get request.
+        /// </summary>
+        /// <value>
+        /// The vclock.
+        /// </value>
+        /// <remarks>Review the information at http://wiki.basho.com/Vector-Clocks.html for additional information on how vector clocks 
+        /// are used in Riak.</remarks>
+        public byte[] Vclock { get; set; }
+        
+        /// <summary>
+        /// The number of replicas that must return before a delete is considered a succes.
+        /// </summary>
+        /// <value>
+        /// The R value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? R { get; set; }
+        
+        /// <summary>
+        /// The number of replicas that must respond before a write is considered a success.
+        /// </summary>
+        /// <value>The W value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.</value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? W { get; set; }
+        
+        /// <summary>
+        /// Primary Read Quorum - the number of replicas that need to be available when retrieving the object.
+        /// </summary>
+        /// <value>
+        /// The primary read quorum. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? Pr { get; set; }
+        
+        /// <summary>
+        /// Primary Write Quorum - the number of replicas need to be available when the write is attempted.
+        /// </summary>
+        /// <value>
+        /// The primary write quorum. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? Pw { get; set; }
+        
+        /// <summary>
+        /// Durable writes - the number of replicas that must commit to durable storage before returning a successful response.
+        /// </summary>
+        /// <value>
+        /// The durable write value. Possible values include 'default', 'one', 'quorum', 'all', or any integer.
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.Constants.QuorumOptions"/></remarks>
+        public uint? Dw { get; set; }                public RiakDeleteOptions() {            Rw = RiakConstants.Defaults.RVal;        }
+        
+        internal void Populate(RpbDelReq request)
+        {
+            if (Rw.HasValue)
+            {
+                request.Rw = Rw.Value;
+            }
+            
+            if (Vclock != null)
+            {
+                request.Vclock = Vclock;
+            }
+            
+            if (R.HasValue)
+            {
+                request.R = R.Value;
+            }
+            
+            if (W.HasValue)
+            {
+                request.W = W.Value;
+            }
+            
+            if (Pr.HasValue)
+            {
+                request.Pr = Pr.Value;
+            }
+            
+            if (Pw.HasValue)
+            {
+                request.Pw = Pw.Value;
+            }
+            
+            if (Dw.HasValue)
+            {
+                request.Dw = Dw.Value;
+            }
+        }
+    }
+}
+
