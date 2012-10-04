0a5d0153ba4ef6f44b5cb49af4fd1679dd8424a3
Taliesin Sisson <taliesins@yahoo.com>
2012-10-04 22:38:05 +0100

    Package nuget

--- build.ps1
+++ build.ps1
@@ -20,7 +20,7 @@ $buildPath = (Resolve-Path $scriptPath\build)
 $psakeModule = @(Get-ChildItem $scriptPath\* -recurse -include psake.ps1)[0].FullName
 . $psakeModule $buildFile $taskList '4.0' $docs $parameters $properties
 . $psakeModule $buildFile $taskList '4.5' $docs $parameters $properties
-#. $psakeModule $buildFile Package $framework $docs $parameters $properties
+. $psakeModule $buildFile Package $framework $docs $parameters $properties
 
 if($env:BUILD_NUMBER) {
   [Environment]::Exit($lastexitcode)
