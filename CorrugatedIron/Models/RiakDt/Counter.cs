b2063d6304a221e2aeecc6b706b5038ca006c8b4
Jeremiah Peschka <jeremiah@brentozar.com>
2013-09-03 20:41:44 -0700

    WIP on #166
    * Create Counter class
    * Create RiakDtUpdateOptions
    * Add supporting bricabrac
    
    
    Former-commit-id: f47b66bb88e4854dda08154f8857f340f8809d73

---
+++ CorrugatedIron/Models/RiakDt/Counter.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) 2011 - OJ Reeves & Jeremiah Peschka
+//
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using CorrugatedIron.Extensions;
+using CorrugatedIron.Messages;
+
+namespace CorrugatedIron.Models.RiakDt
+{
+    public class CounterOp
+    {
+        public long Value { get; private set; }
+
+        public CounterOp(long value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Counter : IRiakDtType<CounterOp>, IChangeTracking 
+    {
+        public bool IsChanged { get; private set; }
+        private long _value;
+        private byte[] _context;
+        private DtFetchResp.DataType _dataType = DtFetchResp.DataType.COUNTER;
+
+        public long Value 
+        { 
+            get
+            {
+                if (IsChanged)
+                {
+                    return _value + _operations.Sum(op => op.Value);
+                }
+
+                return _value;
+            }
+            private set { _value = value; }
+        }
+
+        private readonly List<CounterOp> _operations = new List<CounterOp>();
+
+        public Counter(DtFetchResp response)
+        {
+            Value = response.counter_value;
+            _context = response.context;
+        }
+        
+        public Counter Increment(long value = 1)
+        {
+            _operations.Add(new CounterOp(value));
+            IsChanged = true;
+            return this;
+        }
+
+        public ReadOnlyCollection<CounterOp> Operations
+        {
+            get { return _operations.AsReadOnly(); }
+        }
+
+        public static Counter operator ++(Counter counter)
+        {
+            return counter.Increment();
+        }
+
+        public MapEntry ToMapEntry(string fieldName)
+        {
+            return new MapEntry
+                {
+                    counter_value = Value,
+                    field = new MapField()
+                        {
+                            name = fieldName.ToRiakString(),
+                            type = MapField.MapFieldType.COUNTER
+                        }
+                };
+        }
+
+        public void AcceptChanges()
+        {
+            _value = _value + _operations.Sum(op => op.Value);
+            _operations.Clear();
+            IsChanged = false;
+        }
+
+        
+    }
+
+    public interface IRiakDtType<T> 
+    {
+        ReadOnlyCollection<T> Operations { get; }
+        MapEntry ToMapEntry(string fieldName);
+    }
+}
---
+++ CorrugatedIron/Models/RiakDtFetchOptions.cs
@@ -0,0 +1,187 @@
+﻿// Copyright (c) 2011 - OJ Reeves & Jeremiah Peschka
+//
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Collections.Generic;
+using CorrugatedIron.Containers;
+using CorrugatedIron.Extensions;
+using CorrugatedIron.Messages;
+using CorrugatedIron.Util;
+
+namespace CorrugatedIron.Models
+{
+    public class RiakDtFetchOptions
+    {
+        /// <summary>
+        /// The number of replicas that must return before a fetch is considered a success.
+        /// </summary>
+        /// <value>
+        /// The R value. Possible values include any integer as well as 
+        /// a string value of 'quorum', 'one', 'all', or 'default'. 
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.RiakConstants.QuorumOptions"/></remarks>
+        public Either<uint, string> R { get; private set; }
+
+        /// <summary>
+        /// Primary Read Quorum - the number of replicas that need to be available when retrieving the object.
+        /// </summary>
+        /// <value>
+        /// The primary read quorum. Possible values include any integer as well as 
+        /// a string value of 'quorum', 'one', 'all', or 'default'. 
+        /// </value>
+        /// <remarks>Developers looking for an easy way to set this can look at <see cref="CorrugatedIron.Util.RiakConstants.QuorumOptions"/></remarks>
+        public Either<uint, string> Pr { get; private set; }
+
+        /// <summary>
+        /// Basic Quorum semantics - whether to return early in some failure cases (eg. when r=1 and you get 2 errors and a success basic_quorum=true would return an error)
+        /// </summary>
+        /// <value>
+        /// Whether basic quorum semantics will be used.
+        /// </value>
+        public bool? BasicQuorum { get; private set; }
+
+        /// <summary>
+        /// Should not found responses from Riak be treated as an OK result for a find operation. 
+        /// </summary>
+        /// <value>
+        /// The notfound_ok value.
+        /// </value>
+        public bool? NotFoundOk { get; private set; }
+
+        public uint? Timeout { get; private set; }
+
+        /// <summary>
+        /// Whether a sloppy quorum should be used.
+        /// </summary>
+        /// <remarks><para>Sloppy quorum defers to the first N healthy nodes from the pref list,
+        /// which may not always be the first N nodes encountered in the consistent hash ring.
+        /// In effect - sloppy quorum allows us to trust hand off nodes in the event of a network
+        /// partition or other similarily amusing event.</para>
+        /// <para>Specifying PR will override Sloppy Quorum settings.</para>
+        /// <para>Refer to http://lists.basho.com/pipermail/riak-users_lists.basho.com/2012-January/007157.html for additional details.</para></remarks>
+        public bool? SloppyQuorum { get; private set; }
+
+        public uint? NVal { get; private set; }
+
+        /// <summary>
+        /// Determines whether or not the context will be returned. For read-only requests or context-free operations, you can set this to false to reduce the size of the response payload.
+        /// </summary>
+        public bool? IncludeContext { get; private set; }
+
+        public RiakDtFetchOptions SetR(uint value)
+        {
+            return WriteQuorum(value, var => R = var);
+        }
+
+        public RiakDtFetchOptions SetR(string value)
+        {
+            return WriteQuorum(value, var => R = var);
+        }
+
+        public RiakDtFetchOptions SetPr(uint value)
+        {
+            return WriteQuorum(value, var => Pr = var);
+        }
+
+        public RiakDtFetchOptions SetPr(string value)
+        {
+            return WriteQuorum(value, var => Pr = var);
+        }
+
+        public RiakDtFetchOptions SetBasicQuorum(bool value)
+        {
+            BasicQuorum = value;
+            return this;
+        }
+
+        public RiakDtFetchOptions SetNotFoundOk(bool value)
+        {
+            NotFoundOk = value;
+            return this;
+        }
+
+        public RiakDtFetchOptions SetTimeout(uint value)
+        {
+            Timeout = value;
+            return this;
+        }
+
+        public RiakDtFetchOptions SetSloppyQuorum(bool value)
+        {
+            SloppyQuorum = value;
+            return this;
+        }
+
+        public RiakDtFetchOptions SetNVal(uint value)
+        {
+            NVal = value;
+            return this;
+        }
+
+        public RiakDtFetchOptions()
+        {
+            R = new Either<uint, string>(RiakConstants.QuorumOptions.Default);
+            Pr = new Either<uint, string>(RiakConstants.QuorumOptions.Default);
+        }
+
+        internal void Populate(DtFetchReq request)
+        {
+            request.r = R.IsLeft ? R.Left : R.Right.ToRpbOption();
+            request.pr = Pr.IsLeft ? Pr.Left : Pr.Right.ToRpbOption();
+
+            if (BasicQuorum.HasValue)
+            {
+                request.basic_quorum = BasicQuorum.Value;
+            }
+
+            if (NotFoundOk.HasValue)
+            {
+                request.notfound_ok = NotFoundOk.Value;
+            }
+
+            if (Timeout.HasValue)
+            {
+                request.timeout = Timeout.Value;
+            }
+
+            if (SloppyQuorum.HasValue)
+            {
+                request.sloppy_quorum = SloppyQuorum.Value;
+            }
+
+            if (NVal.HasValue)
+            {
+                request.n_val = NVal.Value;
+            }
+        }
+
+        private RiakDtFetchOptions WriteQuorum(string value, Action<Either<uint, string>> setter)
+        {
+            System.Diagnostics.Debug.Assert(new HashSet<string> { "all", "quorum", "one", "default" }.Contains(value), "Incorrect quorum value");
+
+            setter(new Either<uint, string>(value));
+            return this;
+        }
+
+        private RiakDtFetchOptions WriteQuorum(uint value, Action<Either<uint, string>> setter)
+        {
+            System.Diagnostics.Debug.Assert(value >= 1);
+
+            setter(new Either<uint, string>(value));
+            return this;
+        }
+    }
+}
