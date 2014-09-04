1216e159715719670f2fce120dca596a06831402
Alex Moore <moore.alex@gmail.com>
2014-09-04 10:42:32 -0400

    Update Mono to 3.8

--- .mono-build-scripts/InstallMono.sh
+++ .mono-build-scripts/InstallMono.sh
@@ -2,9 +2,9 @@
 PLATFORM=`uname -s`
 if [ "Darwin" = "$PLATFORM" ]; then
     # OS/X install steps here
-    if [ ! -x /usr/bin/mono ]; then
-        curl -O http://download.mono-project.com/archive/3.6.0/macos-10-x86/MonoFramework-MDK-3.6.0.macos10.xamarin.x86.pkg
-        sudo /usr/sbin/installer -pkg MonoFramework-MDK-3.6.0.macos10.xamarin.x86.pkg -target /
+    if [ ! -x /usr/bin/mono ] || [ -z "$(mono -V | grep 3.8.0)" ]; then
+        curl -O http://download.mono-project.com/archive/3.8.0/macos-10-x86/MonoFramework-MDK-3.8.0.macos10.xamarin.x86.pkg
+        sudo /usr/sbin/installer -pkg MonoFramework-MDK-3.8.0.macos10.xamarin.x86.pkg -target /
     fi
 else # Assume ubuntu/debian
     sudo apt-get install -y python-software-properties
