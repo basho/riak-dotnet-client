bb95be7c6cd6a90cfdf647e4e645dfc94704ecb8
Alex Moore <moore.alex@gmail.com>
2014-09-08 23:02:57 -0400

    Rudimentary Set operations are working.
    Removed optional null context, can now build correct pb objects for
    get/update sets.
    
    
    Former-commit-id: 770d98287b6627dc8c46f7708909fb1ab83474f3

---
+++ CorrugatedIron/Models/RiakDt/SetOperation.cs
@@ -0,0 +1,15 @@
+using CorrugatedIron.Messages;
+
+namespace CorrugatedIron.Models.RiakDt
+{
+    public class SetOperation :IDtOp
+    {
+        public DtOp ToDtOp()
+        {
+            return new DtOp
+            {
+                set_op = new SetOp()
+            };
+        }
+    }
+}
