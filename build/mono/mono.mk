9595cc32593f91f2e1f244ab3ff19c1286404ae4
Luke Bakken <lbakken@basho.com>
2014-11-07 11:34:21 -0800

    add mono.mk file

---
+++ build/mono/mono.mk
@@ -0,0 +1,52 @@
+INSTALL_MONO = $(SLNDIR)/build/mono/install-mono
+
+NUGET_PKGDIR = $(SLNDIR)/packages
+NUGET_EXE = $(SLNDIR)/.nuget/NuGet.exe
+NUGET_CFG = $(SLNDIR)/.nuget/NuGet.Config
+NUGET_RESTORE = $(MONO) $(NUGET_EXE) restore -PackagesDirectory $(NUGET_PKGDIR) -ConfigFile $(NUGET_CFG)
+
+XBUILD = xbuild
+MONO = mono --runtime='v4.0.30319'
+NUNIT = $(SLNDIR)/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -config Debug-Mono -nologo -nodots -labels
+PROTOGEN = ~/bin/ProtoGen/protogen.exe
+PROTOC = protoc
+
+all: release debug
+
+install-certs:
+	mozroots --import --sync
+
+# NB: run this target if restorepkg fails on download
+install-mono:
+	$(INSTALL_MONO)
+
+restorepkg:
+	$(NUGET_RESTORE) $(SLNDIR)/.nuget/packages.config
+	$(NUGET_RESTORE) $(SLNDIR)/CorrugatedIron/packages.config
+	$(NUGET_RESTORE) $(SLNDIR)/CorrugatedIron.Tests/packages.config
+	$(NUGET_RESTORE) $(SLNDIR)/CorrugatedIron.Tests.Live/packages.config
+
+proto:
+	cd CorrugatedIron/Messages
+	$(MONO) $(PROTOGEN) -i:$< -o:$@
+	rm $<
+
+release: restorepkg
+	$(XBUILD) $(SLNDIR)/CorrugatedIron.sln /property:Configuration=Release /property:Mono=True
+
+debug: restorepkg
+	$(XBUILD) $(SLNDIR)/CorrugatedIron.sln /property:Configuration=Debug /property:Mono=True
+
+test-all: unit-test integration-test
+
+unit-test: debug
+	$(MONO) $(NUNIT) -work=$(SLNDIR)/CorrugatedIron.Tests $(SLNDIR)/CorrugatedIron.Tests/CorrugatedIron.Tests.nunit
+
+integration-test: debug unit-test
+	$(MONO) $(NUNIT) -work=$(SLNDIR)/CorrugatedIron.Tests.Live $(SLNDIR)/CorrugatedIron.Tests.Live/CorrugatedIron.Tests.Live.nunit
+
+clean:
+	$(XBUILD) $(SLNDIR)/CorrugatedIron.sln /target:clean /property:Configuration=Debug /property:Mono=True
+	$(XBUILD) $(SLNDIR)/CorrugatedIron.sln /target:clean /property:Configuration=Release /property:Mono=True
+	find $(SLNDIR) -type f \( -name '*.err' -o -name '*.out' \) -delete
+
