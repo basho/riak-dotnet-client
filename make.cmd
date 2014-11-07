39c548b656c58952eafa9b241ee11e7f5bf4d7cb
Luke Bakken <lbakken@basho.com>
2014-11-07 13:05:54 -0800

    consolidate docs, begin work on windows build
    Former-commit-id: 1b815fb7d1cfce7eacb8bc0fb5546bfdb9b52580

--- make.cmd
+++ make.cmd
@@ -1,3 +1,53 @@
-.nuget\NuGet.exe restore -PackagesDirectory .\packages .nuget\packages.config
+@echo off
+setlocal EnableExtensions
 
-%windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe make.msbuild /target:Release
+if (%FrameworkDir%)==() set FrameworkDir=%WINDIR%\Microsoft.NET\Framework\
+if (%FrameworkVersion%)==() set FrameworkVersion=v4.0.30319
+
+rem NB: %~dp0 ends in a backslash
+set CURDIR=%~dp0
+set NUGETEXE=%CURDIR%\.nuget\NuGet.exe
+if not exist %NUGETEXE% goto ERR_NUGETEXE
+
+set MSBUILDEXE=%FrameworkDir%%FrameworkVersion%\msbuild.exe
+if not exist %MSBUILDEXE% goto ERR_MSBUILD
+
+set TARGET=%1
+if (%TARGET%)==() set TARGET=Debug
+
+set VERBOSITY=%2
+if (%VERBOSITY%)==() set VERBOSITY=normal
+
+%NUGETEXE% restore -PackagesDirectory %CURDIR%\packages .nuget\packages.config
+if errorlevel 1 goto ERR_FAILED
+
+%MSBUILDEXE% /verbosity:%VERBOSITY% /nologo /m /property:SolutionDir=%CURDIR% /t:%TARGET% %CURDIR%\build\mswin\build.proj
+if errorlevel 1 goto ERR_FAILED
+
+echo.
+echo ************************
+echo *** BUILD SUCCESSFUL ***
+echo ************************
+echo.
+goto :EOF
+
+:ERR_FAILED
+echo.
+echo ********************
+echo *** BUILD FAILED ***
+echo ********************
+echo.
+exit /b 1
+
+:ERR_NUGETEXE
+echo.
+echo. ERROR: Can't find NuGet.exe (expected: %NUGETEXE%)
+echo.
+exit /b 1
+
+:ERR_MSBUILD
+echo.
+echo. ERROR: Can't find MSBuild.exe (expected: %MSBUILDEXE%)
+echo.
+exit /b 1
+
