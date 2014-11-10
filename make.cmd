18e66ee1e87db49a6e65d2390c23b0addbf6a521
Luke Bakken <lbakken@basho.com>
2014-11-10 12:58:55 -0800

    Align build targets with Mono Makefile

--- make.cmd
+++ make.cmd
@@ -12,25 +12,23 @@ if not exist %NUGETEXE% goto ERR_NUGETEXE
 set MSBUILDEXE=%FrameworkDir%%FrameworkVersion%\msbuild.exe
 if not exist %MSBUILDEXE% goto ERR_MSBUILD
 
-rem Targets:
-rem Build - Normal build of a Configuration
-rem Clean - Clean everything
-rem PBuild - Parallel build of all Configurations
-rem ProtoGen - Rebuild cs files from proto files
 set TARGET=%1
-if (%TARGET%)==() set TARGET=Build
+if (%TARGET%)==(Help) goto SHOW_HELPMSG
+if (%TARGET%)==(help) goto SHOW_HELPMSG
+if (%TARGET%)==(-h) goto SHOW_HELPMSG
+if (%TARGET%)==(/h) goto SHOW_HELPMSG
+if (%TARGET%)==(-?) goto SHOW_HELPMSG
+if (%TARGET%)==(/?) goto SHOW_HELPMSG
+if (%TARGET%)==() set TARGET=Debug
 
-set CONFIGURATION=%2
-if (%CONFIGURATION%)==() set CONFIGURATION=Release
-
-set VERBOSITY=%3
+set VERBOSITY=%2
 if (%VERBOSITY%)==() set VERBOSITY=Normal
 
 %NUGETEXE% restore -PackagesDirectory %CURDIR%\packages .nuget\packages.config
 if errorlevel 1 goto ERR_FAILED
 
 rem NB: this will always do build for Release *and* Debug configuration
-%MSBUILDEXE% /verbosity:%VERBOSITY% /nologo /m /property:SolutionDir=%CURDIR% /property:Configuration=%CONFIGURATION% /t:%TARGET% %CURDIR%\build\build.proj
+%MSBUILDEXE% /verbosity:%VERBOSITY% /nologo /m /property:SolutionDir=%CURDIR% /t:%TARGET% %CURDIR%\build\build.proj
 if errorlevel 1 goto ERR_FAILED
 
 echo.
@@ -59,4 +57,17 @@ echo.
 echo. ERROR: Can't find MSBuild.exe (expected: %MSBUILDEXE%)
 echo.
 exit /b 1
+
+:SHOW_HELPMSG
+echo.
+echo Targets:
+echo --------------------------------------------
+echo Debug    - Debug build
+echo Release  - Release build with versioning
+echo All      - Parallel build of Debug, Release
+echo Clean    - Clean everything
+echo ProtoGen - Rebuild cs files from proto files
+echo --------------------------------------------
+echo.
+exit /b 0
 
