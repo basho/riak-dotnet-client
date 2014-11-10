2e2b7c8fed2d0d4ccc3cfe12b2441e748e2f8e63
Luke Bakken <lbakken@basho.com>
2014-11-10 10:50:03 -0800

    Add target for rebuild proto messages from proto files.

--- tools/regen_proto.bat
+++
@@ -1,9 +0,0 @@
-cd ..\src\CorrugatedIron\Messages
-
-"C:\Program Files (x86)\protobuf-net\protobuf-net-VS9\protogen.exe" -ns:CorrugatedIron.Messages -i:riak.proto -o:riak.cs
-"C:\Program Files (x86)\protobuf-net\protobuf-net-VS9\protogen.exe" -ns:CorrugatedIron.Messages -i:riak_dt.proto -o:riak_dt.cs
-"C:\Program Files (x86)\protobuf-net\protobuf-net-VS9\protogen.exe" -ns:CorrugatedIron.Messages -i:riak_kv.proto -o:riak_kv.cs
-"C:\Program Files (x86)\protobuf-net\protobuf-net-VS9\protogen.exe" -ns:CorrugatedIron.Messages -i:riak_search.proto -o:riak_search.cs
-"C:\Program Files (x86)\protobuf-net\protobuf-net-VS9\protogen.exe" -ns:CorrugatedIron.Messages -i:riak_yokozuna.proto -o:riak_yokozuna.cs
-
-cd ..\..\..
