d4418c6e12b3968204e3e5011bfdb4116dcddadb
Alex Moore <moore.alex@gmail.com>
2014-11-25 12:23:04 -0500

    Moving call to mozroots
    Former-commit-id: 7acb006499dff8f8e172685b6a20005d425ab8e4

--- .mono-build-scripts/InstallMono.sh
+++ .mono-build-scripts/InstallMono.sh
@@ -7,9 +7,9 @@ if [ "Darwin" = "$PLATFORM" ]; then
         sudo /usr/sbin/installer -pkg MonoFramework-MDK-3.8.0.macos10.xamarin.x86.pkg -target /
     fi
 else # Assume ubuntu/debian
-	mozroots --import --sync
-    sudo apt-get install -y python-software-properties
+	sudo apt-get install -y python-software-properties
     sudo apt-add-repository -y ppa:directhex/monoxide
     sudo apt-get update
     sudo apt-get install -y mono-devel=3.2.1+dfsg-1~pre2 mono-gmcs=3.2.1+dfsg-1~pre2 nunit-console
+    mozroots --import --sync
 fi
