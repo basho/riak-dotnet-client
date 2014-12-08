d7b511a2bfdd2f0f803392047979e7713b068dfc
Alex Moore <moore.alex@gmail.com>
2014-12-08 13:36:49 -0500

    Adding more complex yokozuna search tests.

--- src/CorrugatedIron.Tests.Live/Search/TestSearchOperation.cs
+++ src/CorrugatedIron.Tests.Live/Search/TestSearchOperation.cs
@@ -1,21 +1,5 @@
-﻿// Copyright (c) 2010 - OJ Reeves & Jeremiah Peschka
-// 
-// This file is provided to you under the Apache License,
-// Version 2.0 (the "License"); you may not use this file
-// except in compliance with the License.  You may obtain
-// a copy of the License at
-// 
-//    http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-using System;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
 using CorrugatedIron.Extensions;
 using CorrugatedIron.Models;
 using CorrugatedIron.Models.Search;
@@ -29,13 +13,10 @@ namespace CorrugatedIron.Tests.Live.Search
     [TestFixture]
     public class TestSearchOperation : LiveRiakConnectionTestBase
     {
-        public TestSearchOperation()
-            : base("riak1NodeConfiguration") { }
-
-        [SetUp]
-        public new void SetUp()
+        [TestFixtureSetUp]
+        public void Init()
         {
-            base.SetUp();
+            SetUp();
 
             var index = new SearchIndex(Index);
             Client.PutSearchIndex(index);
@@ -48,13 +29,20 @@ namespace CorrugatedIron.Tests.Live.Search
             Func<RiakResult<RiakBucketProperties>> setBucketProperties =
                 () =>
                 {
-                    var props = Client.GetBucketProperties(BucketType, Bucket).Value;
+                    RiakBucketProperties props = Client.GetBucketProperties(BucketType, Bucket).Value;
                     props.SetSearchIndex(Index);
                     Client.SetBucketProperties(BucketType, Bucket, props);
                     return Client.GetBucketProperties(BucketType, Bucket);
                 };
 
             setBucketProperties.WaitUntil(indexIsSet);
+
+            PrepSearch();
+        }
+
+        public TestSearchOperation()
+            : base("riak1NodeConfiguration")
+        {
         }
 
         private const string BucketType = "search_type";
@@ -63,28 +51,31 @@ namespace CorrugatedIron.Tests.Live.Search
         private const string RiakSearchKey = "a.hacker";
         private const string RiakSearchKey2 = "a.public";
         private readonly Random _random = new Random();
+        private int _randomId;
+        private RiakObjectId _alyssaRiakId;
 
-        private string RiakSearchDoc =
-            "{{\"name_s\":\"Alyssa{0}P. Hacker\", \"age_i\":35, \"leader_b\":true, \"bio_s\":\"I'm an engineer, making awesome things.\", \"favorites_s\":{{\"book_s\":\"The Moon is a Harsh Mistress\",\"album_s\":\"Magical Mystery Tour\", }}}}";
+        // See https://raw.githubusercontent.com/basho/yokozuna/develop/priv/default_schema.xml for dynamic field suffix meanings.
+        private const string RiakSearchDoc =
+            "{{\"name_s\":\"{0}Alyssa P. Hacker\", \"age_i\":35, \"leader_b\":true, \"bio_tsd\":\"I'm an engineer, making awesome things.\", \"favorites\":{{\"book_tsd\":\"The Moon is a Harsh Mistress\",\"album_tsd\":\"Magical Mystery Tour\" }}}}";
 
-        private string RiakSearchDoc2 =
-            "{{\"name_s\":\"Alan{0} Q. Public\", \"age_i\":38, \"bio_s\":\"I'm an exciting awesome mathematician\", \"favorites_s\":{{\"book_s\":\"Prelude to Mathematics\",\"album_s\":\"The Fame Monster\"}}}}";
+        private const string RiakSearchDoc2 =
+            "{{\"name_s\":\"{0}Alan Q. Public\", \"age_i\":38, \"bio_tsd\":\"I'm an exciting awesome mathematician\", \"favorites\":{{\"book_tsd\":\"Prelude to Mathematics\",\"album_tsd\":\"The Fame Monster\"}}}}";
 
-        [Test]
-        public void SearchingWithSimpleFluentQueryWorksCorrectly()
+
+        private void PrepSearch()
         {
-            var randomId = _random.Next();
-            var alyssaKey = RiakSearchKey + randomId;
-            var alyssaRiakId = new RiakObjectId(BucketType, Bucket, alyssaKey);
-            var alyssaDoc = String.Format(RiakSearchDoc, randomId);
+            _randomId = _random.Next();
+            var alyssaKey = _randomId + RiakSearchKey;
+            _alyssaRiakId = new RiakObjectId(BucketType, Bucket, alyssaKey);
+            var alyssaDoc = String.Format(RiakSearchDoc, _randomId);
 
-            var alanKey = RiakSearchKey2 + randomId;
+            var alanKey = _randomId + RiakSearchKey2;
             var alanRiakId = new RiakObjectId(BucketType, Bucket, alanKey);
-            var alanDoc = String.Format(RiakSearchDoc2, randomId);
+            var alanDoc = String.Format(RiakSearchDoc2, _randomId);
 
-            Console.WriteLine("Using {0}, {1} for SearchingWithSimpleFluentQueryWorksCorrectly() key", alyssaKey, alanKey);
+            Console.WriteLine("Using {0}, {1} for Yokozuna search keys", alyssaKey, alanKey);
 
-            var put1Result = Client.Put(new RiakObject(alyssaRiakId, alyssaDoc.ToRiakString(),
+            var put1Result = Client.Put(new RiakObject(_alyssaRiakId, alyssaDoc.ToRiakString(),
                 RiakConstants.ContentTypes.ApplicationJson, RiakConstants.CharSets.Utf8));
 
             var put2Result = Client.Put(new RiakObject(alanRiakId, alanDoc.ToRiakString(),
@@ -92,29 +83,125 @@ namespace CorrugatedIron.Tests.Live.Search
 
             put1Result.IsSuccess.ShouldBeTrue(put1Result.ErrorMessage);
             put2Result.IsSuccess.ShouldBeTrue(put2Result.ErrorMessage);
+        }
 
+        [Test]
+        public void SearchingWithSimpleFluentQueryWorksCorrectly()
+        {
             var req = new RiakSearchRequest
             {
                 Query = new RiakFluentSearch(Index, "name_s")
-                                .Search("Alyssa" + randomId + "*")
-                                .Build()
+                    .Search(_randomId + "Alyssa P. Hacker")
+                    .Build()
             };
-
-            Func<RiakResult<RiakSearchResult>, bool> matchIsFound =
-                result => result.IsSuccess &&
-                          result.Value != null &&
-                          result.Value.NumFound > 0;
-
-            Func<RiakResult<RiakSearchResult>> runSolrQuery =
-                () => Client.Search(req);
-
-            var searchResult = runSolrQuery.WaitUntil(matchIsFound);
+            
+            var searchResult = RunSolrQuery(req).WaitUntil(MatchIsFound);
 
             searchResult.IsSuccess.ShouldBeTrue(searchResult.ErrorMessage);
             searchResult.Value.NumFound.ShouldEqual(1u);
             searchResult.Value.Documents.Count.ShouldEqual(1);
-            searchResult.Value.Documents[0].Fields.Count.ShouldEqual(6); // [ name_s, age_i, leader_b, bio_s favorites_s.book_s, favorites_s.album_s ]
-            searchResult.Value.Documents[0].RiakObjectId.ShouldEqual(alyssaRiakId);
+
+            // [ _yz_rt, _yz_rb, _yz_rk, score, _yz_id ]
+            // [ name_s, age_i, leader_b, bio_tsd, favorites.book_tsd , favorites.album_tsd ]
+            searchResult.Value.Documents[0].Fields.Count.ShouldEqual(11);
+            searchResult.Value.Documents[0].RiakObjectId.ShouldEqual(_alyssaRiakId);
+        }
+
+        [Test]
+        public void SearchingWithWildcardFluentQueryWorksCorrectly()
+        {
+            var req = new RiakSearchRequest
+            {
+                Query = new RiakFluentSearch(Index, "name_s").Search(Token.StartsWith(_randomId + "Al")).Build()
+            };
+
+            var searchResult = RunSolrQuery(req).WaitUntil(MatchIsFound);
+            searchResult.IsSuccess.ShouldBeTrue(searchResult.ErrorMessage);
+            searchResult.Value.NumFound.ShouldEqual(2u);
+            searchResult.Value.Documents.Count.ShouldEqual(2);
+        }
+
+        [Test]
+        public void SearchingWithMoreComplexFluentQueryWorksCorrectly()
+        {
+            var req = new RiakSearchRequest
+            {
+                Query = new RiakFluentSearch(Index, "name_s")
+            };
+
+            // name_s:{integer}Al* AND bio_tsd:awesome AND bio_tsd:an AND (bio_tsd:mathematician OR favorites.album_tsd:Fame)
+            req.Query.Search(Token.StartsWith(_randomId + "Al"))
+                .And("bio_tsd", "awesome")
+                .And("an")
+                .And("mathematician", s => s.Or("favorites.album_tsd", "Fame"));
+
+            Console.WriteLine(req.Query.ToString());
+            
+            var result = RunSolrQuery(req).WaitUntil(MatchIsFound);
+
+            result.IsSuccess.ShouldBeTrue(result.ErrorMessage);
+            result.Value.NumFound.ShouldEqual(1u);
+            result.Value.Documents.Count.ShouldEqual(1);
+            
+            // [ _yz_rt, _yz_rb, _yz_rk, score, _yz_id ]
+            // [ name_s, age_i, leader_b, bio_tsd, favorites.book_tsd , favorites.album_tsd ]
+            result.Value.Documents[0].Fields.Count.ShouldEqual(10);
+            var id = result.Value.Documents[0].Id;
+            id.Contains("a.public").ShouldBeTrue(string.Format("{0} does not contain {1}", id, "a.public"));
+        }
+
+        [Test]
+        public void SettingFieldListReturnsOnlyFieldsSpecified()
+        {
+            var req = new RiakSearchRequest
+            {
+                Query = new RiakFluentSearch(Index, "name_s"),
+                PreSort = PreSort.Key,
+                DefaultOperation = DefaultOperation.Or,
+                ReturnFields = new List<string>
+                {
+                    RiakConstants.SearchFieldKeys.Id,
+                    "bio_tsd",
+                    "favorites.album_tsd"
+                }
+            };
+
+
+            // name_s:{integer}Al* AND bio_tsd:awesome AND bio_tsd:an AND (bio_tsd:mathematician OR favorites.album_tsd:Fame)
+            req.Query.Search(Token.StartsWith(_randomId + "Al"))
+                .And("bio_tsd", "awesome")
+                .And("an")
+                .And("mathematician", s => s.Or("favorites.album_tsd", "Fame"));
+            
+
+            Console.WriteLine(req.Query.ToString());
+
+            var result = RunSolrQuery(req).WaitUntil(MatchIsFound);
+            result.IsSuccess.ShouldBeTrue(result.ErrorMessage);
+            result.Value.NumFound.ShouldEqual(1u);
+            result.Value.Documents.Count.ShouldEqual(1);
+            result.Value.Documents[0].Fields.Count.ShouldEqual(3);
+            result.Value.Documents[0].Id.ShouldNotBeNull();
+        }
+
+
+        private Func<RiakResult<RiakSearchResult>> RunSolrQuery(RiakSearchRequest req)
+        {
+            Func<RiakResult<RiakSearchResult>> runSolrQuery =
+                () => Client.Search(req);
+            return runSolrQuery;
+        }
+        
+        private static Func<RiakResult<RiakSearchResult>, bool> MatchIsFound
+        {
+            get
+            {
+                Func<RiakResult<RiakSearchResult>, bool> matchIsFound =
+                    result => result.IsSuccess &&
+                              result.Value != null &&
+                              result.Value.NumFound > 0;
+                return matchIsFound;
+            }
         }
     }
 }
