fb5f693a253d9661ef4981df1a20cb080700346a
Alex Moore <moore.alex@gmail.com>
2014-12-16 15:56:50 -0500

    Edit helper output, add more retries.
    Former-commit-id: d9aac2d6a619d49395a7fcd80e66e96fa1557d23

---
+++ src/CorrugatedIron.Tests/Extensions/UnitTestExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using CorrugatedIron.Tests.Extensions;
+
+namespace CorrugatedIron.Tests
+{
+    [TestFixture]
+    public class UnitTestExtensionsTest
+    {
+        [Ignore("Run this to test the WaitUntil test helper's output")]
+        [Test]
+        public void ThisTestShouldFail()
+        {
+            Func<RiakResult> alwaysFail = () => RiakResult.Error(ResultCode.InvalidRequest, "Nope.", true);
+            Func<RiakResult> alwaysThrow = () => { throw new ApplicationException("Whoopsie"); };
+            var failResult = alwaysFail.WaitUntil(2);
+            alwaysThrow.WaitUntil(2);
+            failResult.IsSuccess.ShouldBeFalse();
+        }
+    }
+}
+
