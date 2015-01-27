7f25d1a19defa06ea49e77d05cd4148baf07c134
Luke Bakken <lbakken@basho.com>
2015-01-27 12:31:13 -0800

    Bug fixes after testing.

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -51,8 +51,6 @@ $github_api_key_file = Resolve-Path '~/.ghapi'
 $github_api_key = Get-Content $github_api_key_file
 Write-Debug "GitHub API Key '$github_api_key'"
 
-$headers = @{ Authorization = "token $github_api_key" }
-
 $release_info = New-Object PSObject -Property @{
         tag_name = $VersionString
         target_commitish = "master"
@@ -62,18 +60,32 @@ $release_info = New-Object PSObject -Property @{
         prerelease = $true
     }
 
+$headers = @{ Authorization = "token $github_api_key" }
+
 $release_json = ConvertTo-Json -InputObject $release_info -Compress
 
-$response = Invoke-WebRequest -Headers $headers -ContentType 'application/json' -Method Post -Body $release_json -Uri 'https://api.github.com/repos/basho-labs/riak-dotnet-client/releases'
-if (!($response.StatusCode == 201)) {
-    throw "Creating release failed: $response.StatusCode"
+try {
+    $response = Invoke-WebRequest -Headers $headers -ContentType 'application/json' -Method Post -Body $release_json -Uri 'https://api.github.com/repos/basho-labs/riak-dotnet-client/releases'
+    if ([int]$response.StatusCode -ne 201) {
+        throw "Creating release failed: $response.StatusCode"
+    }
+}
+catch {
+    if ([int]$_.Exception.Response.StatusCode -eq 422) {
+        Write-Error -Category InvalidOperation "Release already exists in GitHub!"
+        exit 1
+    }
+    else {
+        throw
+    }
 }
 
 $response_json = ConvertFrom-Json -InputObject $response.Content
-$assets_url_with_name = $response_json.assets_url + '?name=' + $release_zip_name
+# https://uploads.github.com/repos/basho-labs/riak-dotnet-client/releases/890350/assets{?name}
+$upload_url_with_name = $response_json.upload_url -Replace '{\?name}', ('?name=' + $release_zip_name)
 
-$response = Get-Content $release_zip_path | Invoke-WebRequest -Headers $headers -ContentType 'application/zip' -Method Post -Uri $assets_url_with_name
-if (!($response.StatusCode == 201)) {
-    throw "Creating release failed: $response.StatusCode"
+$response = Get-Content $release_zip_path | Invoke-WebRequest -Headers $headers -ContentType 'application/zip' -Method Post -Uri $upload_url_with_name
+if ([int]$response.StatusCode -ne 201) {
+    throw "Creating release asset failed: $response.StatusCode"
 }
 
