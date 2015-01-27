d0a2adc316f5801659950351db513c34472dfe56
Luke Bakken <lbakken@basho.com>
2015-01-27 08:52:43 -0800

    Add step to Publish target to create GitHub release.
    Former-commit-id: 31de891545d24dc5b5846e2f0bdf01a6e74b48e2

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -21,22 +21,26 @@ Param(
 
 Set-StrictMode -Version Latest
 
+# Note:
+# Set to Continue to see DEBUG messages
+# $DebugPreference = 'SilentlyContinue'
+
 function Get-ScriptPath {
-	$scriptDir = Get-Variable PSScriptRoot -ErrorAction SilentlyContinue | ForEach-Object { $_.Value }
-	if (!$scriptDir) {
-		if ($MyInvocation.MyCommand.Path) {
-			$scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-		}
-	}
-	if (!$scriptDir) {
-		if ($ExecutionContext.SessionState.Module.Path) {
-			$scriptDir = Split-Path (Split-Path $ExecutionContext.SessionState.Module.Path)
-		}
-	}
-	if (!$scriptDir) {
-		$scriptDir = $PWD
-	}
-	return $scriptDir
+  $scriptDir = Get-Variable PSScriptRoot -ErrorAction SilentlyContinue | ForEach-Object { $_.Value }
+  if (!$scriptDir) {
+    if ($MyInvocation.MyCommand.Path) {
+      $scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+    }
+  }
+  if (!$scriptDir) {
+    if ($ExecutionContext.SessionState.Module.Path) {
+      $scriptDir = Split-Path (Split-Path $ExecutionContext.SessionState.Module.Path)
+    }
+  }
+  if (!$scriptDir) {
+    $scriptDir = $PWD
+  }
+  return $scriptDir
 }
 
 $release_zip_name = 'RiakClient.zip'
@@ -61,13 +65,13 @@ $release_info = New-Object PSObject -Property @{
 $release_json = ConvertTo-Json -InputObject $release_info -Compress
 
 $response = Invoke-WebRequest -Headers $headers -ContentType 'application/json' -Method Post -Body $release_json -Uri 'https://api.github.com/repos/basho-labs/riak-dotnet-client/releases'
-
 if (!($response.StatusCode == 201)) {
     throw "Creating release failed: $response.StatusCode"
 }
 
 $response_json = ConvertFrom-Json -InputObject $response.Content
 $assets_url_with_name = $response_json.assets_url + '?name=' + $release_zip_name
+
 $response = Get-Content $release_zip_path | Invoke-WebRequest -Headers $headers -ContentType 'application/zip' -Method Post -Uri $assets_url_with_name
 if (!($response.StatusCode == 201)) {
     throw "Creating release failed: $response.StatusCode"
