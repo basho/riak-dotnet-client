3021ab14e47875991e5ca34cf14510e6a15687ab
Luke Bakken <lbakken@basho.com>
2015-01-30 12:18:08 -0800

    Ensure that the auto-generated protobuf files are skipped by StyleCop
    Former-commit-id: 8b60aaf7020fd903cc0e0c9444791b1dc9820014

--- src/RiakClient/Comms/IRiakConnectionManager.cs
+++ src/RiakClient/Comms/IRiakConnectionManager.cs
@@ -17,13 +17,14 @@
 // under the License.
 // </copyright>
 
-using System;
-
 namespace RiakClient.Comms
 {
+    using System;
+
     internal interface IRiakConnectionManager : IDisposable
     {
         Tuple<bool, TResult> Consume<TResult>(Func<IRiakConnection, TResult> consumer);
+
         Tuple<bool, TResult> DelayedConsume<TResult>(Func<IRiakConnection, Action, TResult> consumer);
     }
 }
--- src/RiakClient/Comms/IRiakNode.cs
+++ src/RiakClient/Comms/IRiakNode.cs
@@ -25,6 +25,7 @@ namespace RiakClient.Comms
     public interface IRiakNode : IDisposable
     {
         RiakResult UseConnection(Func<IRiakConnection, RiakResult> useFun);
+
         RiakResult<TResult> UseConnection<TResult>(Func<IRiakConnection, RiakResult<TResult>> useFun);
 
         RiakResult<IEnumerable<TResult>> UseDelayedConnection<TResult>(Func<IRiakConnection, Action, RiakResult<IEnumerable<TResult>>> useFun)
