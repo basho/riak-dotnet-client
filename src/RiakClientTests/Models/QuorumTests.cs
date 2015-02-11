922d6e03b613aa722183e32ef44cfc13bb534ccf
Luke Bakken <lbakken@basho.com>
2015-02-11 07:01:31 -0800

    Quorum and QuorumTests are complete
    Former-commit-id: 569da6f8a881102a8baa1f5963085456aa3e6280

--- src/RiakClientTests/Models/QuorumTests.cs
+++ src/RiakClientTests/Models/QuorumTests.cs
@@ -29,9 +29,8 @@ namespace RiakClientTests.Models
         [Test]
         public void WhenUsingInvalidQuorumString_ThrowsArgumentException()
         {
-            Assert.Catch<ArgumentNullException>(() => new Quorum(string.Empty), "Expected ArgumentNullException");
-            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum("frazzle"), "Expected ArgumentOutOfRangeException");
-            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum(-32), "Expected ArgumentOutOfRangeException");
+            Assert.Catch<ArgumentNullException>(() => new Quorum(string.Empty));
+            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum("frazzle"));
         }
 
         [Test]
@@ -39,46 +38,70 @@ namespace RiakClientTests.Models
         {
             var q = new Quorum(0);
             Assert.IsNotNull(q);
-            Assert.AreEqual(0, (int)q);
+        }
+
+        [Test]
+        public void NegOne_IsInvalidQuorum()
+        {
+            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum(-1));
+        }
+
+        [Test]
+        public void NegTwoThroughNegFive_AreValidQuorums()
+        {
+            for (int i = -2; i >= -5; --i)
+            {
+                var q = new Quorum(i);
+                Assert.AreEqual(i, q);
+            }
+        }
+
+        [Test]
+        public void OtherNegValues_AreInvalidQuorums()
+        {
+            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum(-32));
+            Assert.Catch<ArgumentOutOfRangeException>(() => new Quorum(-1024));
         }
 
         [Test]
         public void WhenUsingValidQuorumString_ResultsInValidQuorumValue()
         {
-            var validQuorumData = new Dictionary<string, int>
+            var validQuorumData = new Dictionary<string, int[]>
             {
-                { "one", 1 },
-                { "One", 1 },
-                { "ONE", 1 },
-                { "onE", 1 },
-                { "quorum", 2 },
-                { "Quorum", 2 },
-                { "QUORUM", 2 },
-                { "quOrUm", 2 },
-                { "all", 3 },
-                { "All", 3 },
-                { "ALL", 3 },
-                { "alL", 3 },
-                { "default", 4 },
-                { "Default", 4 },
-                { "DEFAULT", 4 },
-                { "deFaulT", 4 }
+                { "one", new[] { 1, -2 } },
+                { "One", new[] { 1, -2 } },
+                { "ONE", new[] { 1, -2 } },
+                { "onE", new[] { 1, -2 } },
+                { "quorum", new[] { 2, -3 } },
+                { "Quorum", new[] { 2, -3 } },
+                { "QUORUM", new[] { 2, -3 } },
+                { "quOrUm", new[] { 2, -3 } },
+                { "all", new[] { 3, -4 } },
+                { "All", new[] { 3, -4 } },
+                { "ALL", new[] { 3, -4 } },
+                { "alL", new[] { 3, -4 } },
+                { "default", new[] { 4, -5 } },
+                { "Default", new[] { 4, -5 } },
+                { "DEFAULT", new[] { 4, -5 } },
+                { "deFaulT", new[] { 4, -5 } }
             };
 
             foreach (var vqd in validQuorumData)
             {
                 string quorum_str = vqd.Key;
-                int quorum_int = vqd.Value;
-                uint quorum_uint = uint.MaxValue - (uint)quorum_int;
+                int quorum_int_base = vqd.Value[0];
+                int quorum_as_int = vqd.Value[1];
+                uint quorum_uint = uint.MaxValue - (uint)quorum_int_base;
 
                 var quorum = new Quorum(quorum_str);
-                Assert.AreEqual(quorum_int, (int)quorum);
+                Assert.AreEqual(quorum_as_int, (int)quorum);
                 Assert.AreEqual(quorum_uint, (uint)quorum);
                 Assert.AreEqual(quorum_str.ToLowerInvariant(), (string)quorum);
                 Assert.AreEqual(quorum_str.ToLowerInvariant(), quorum.ToString());
 
-                var quorum_from_int = new Quorum(quorum_int);
+                var quorum_from_int = new Quorum(quorum_as_int);
                 Assert.AreEqual(quorum_from_int, quorum);
+                Assert.True(quorum_from_int.Equals(quorum));
             }
         }
     }
