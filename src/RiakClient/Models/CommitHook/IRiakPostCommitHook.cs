7cbd7f644c24ecf98cee66758ff0dc956a3e9b32
Alex Moore <moore.alex@gmail.com>
2015-03-02 15:03:50 -0500

    Doc'd the Models.CommitHook namespace.
    Former-commit-id: f49766a17d1c02ae92f034d9979161165ed1e2d3

--- src/RiakClient/Models/CommitHook/IRiakPostCommitHook.cs
+++ src/RiakClient/Models/CommitHook/IRiakPostCommitHook.cs
@@ -19,6 +19,9 @@
 
 namespace RiakClient.Models.CommitHook
 {
+    /// <summary>
+    /// Represents an interface for post-commit hooks.
+    /// </summary>
     public interface IRiakPostCommitHook : IRiakCommitHook
     {
     }
--- src/RiakClient/Models/CommitHook/IRiakPreCommitHook.cs
+++ src/RiakClient/Models/CommitHook/IRiakPreCommitHook.cs
@@ -19,6 +19,9 @@
 
 namespace RiakClient.Models.CommitHook
 {
+    /// <summary>
+    /// Represents an interface for pre-commit hooks.
+    /// </summary>
     public interface IRiakPreCommitHook : IRiakCommitHook
     {
     }
