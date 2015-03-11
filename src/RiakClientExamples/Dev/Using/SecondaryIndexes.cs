01efdb0c9061af6d091315d21758e8c9143e9b08
Luke Bakken <luke@bakken.io>
2015-03-11 06:34:33 -0700

    2i examples are complete.

--- src/RiakClientExamples/Dev/Using/SecondaryIndexes.cs
+++ src/RiakClientExamples/Dev/Using/SecondaryIndexes.cs
@@ -12,18 +12,21 @@
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
+// specific language governing permidssions and limitations
 // under the License.
 // </copyright>
 
 namespace RiakClientExamples.Dev.Using
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
     using System.Numerics;
     using NUnit.Framework;
     using RiakClient;
     using RiakClient.Models;
+    using RiakClient.Models.Index;
 
     public sealed class SecondaryIndexes : ExampleBase
     {
@@ -60,6 +63,147 @@ namespace RiakClientExamples.Dev.Using
 
         [Test]
         public void IndexingFourObjects()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field1");
+            var rslt = client.GetSecondaryIndex(idxId, "val1", "val4");
+            CheckResult(rslt);
+            PrintKeys(rslt.Value);
+            DeleteObjects(ids);
+        }
+
+        [Test]
+        public void InvalidFieldNamesAndTypes()
+        {
+            id = new RiakObjectId("indexes", "mykey", "test value");
+            var obj = new RiakObject("indexes", "mykey", "test value");
+            var intIdx = obj.IntIndex("test-int-idx");
+            Assert.Throws<FormatException>(() => intIdx.Add("invalid-value"));
+        }
+
+        [Test]
+        public void QueryingExactMatch()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field1");
+            var rslt = client.GetSecondaryIndex(idxId, "val1");
+            CheckResult(rslt);
+            Assert.AreEqual(1, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+        }
+
+        [Test]
+        public void QueryingIntegerIndex()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field2");
+            var rslt = client.GetSecondaryIndex(idxId, 1001);
+            CheckResult(rslt);
+            Assert.AreEqual(1, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+        }
+
+        [Test]
+        public void QueryingBinaryRange()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field1");
+            var rslt = client.GetSecondaryIndex(idxId, "val2", "val4");
+            CheckResult(rslt);
+            Assert.AreEqual(3, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+        }
+
+        [Test]
+        public void QueryingIntegerRange()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field2");
+            var rslt = client.GetSecondaryIndex(idxId, 1002, 1004);
+            CheckResult(rslt);
+            Assert.AreEqual(3, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+        }
+
+        [Test]
+        public void QueryingIntegerRangeWithReturnTerms()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field2");
+            var options = new RiakIndexGetOptions();
+            options.SetReturnTerms(true);
+            var rslt = client.GetSecondaryIndex(idxId, 1002, 1004, options);
+            CheckResult(rslt);
+            Assert.AreEqual(3, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value, printTerms: true);
+        }
+
+        [Test]
+        public void Pagination()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field2");
+            var options = new RiakIndexGetOptions();
+            options.SetMaxResults(2);
+            var rslt = client.GetSecondaryIndex(idxId, 1002, 1004, options);
+            CheckResult(rslt);
+            Assert.AreEqual(2, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+
+            options.SetContinuation(rslt.Continuation);
+            rslt = client.GetSecondaryIndex(idxId, 1002, 1004, options);
+            CheckResult(rslt);
+            Assert.AreEqual(1, rslt.Value.IndexKeyTerms.Count());
+            PrintKeys(rslt.Value);
+        }
+
+        [Test]
+        public void Streaming()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "field2");
+            var rslt = client.StreamGetSecondaryIndex(idxId, 1001, 1004);
+            CheckResult(rslt);
+
+            ushort count = 0;
+            foreach (var kt in rslt.Value.IndexKeyTerms)
+            {
+                PrintKeyTerm(kt);
+                ++count;
+            }
+            Assert.AreEqual(4, count);
+        }
+
+        [Test]
+        public void ReturnKeysViaDollarBucketIndex()
+        {
+            ids = PutObjectsWithIndexValues();
+
+            var idxId = new RiakIndexId("indexes", "people", "$bucket");
+            var rslt = client.GetSecondaryIndex(idxId, "_");
+            CheckResult(rslt);
+            PrintKeys(rslt.Value);
+        }
+
+        private void PutJohnSmith()
+        {
+            id = new RiakObjectId("default", "users", "john_smith");
+            var obj = new RiakObject(id, "...user data...",
+                RiakConstants.ContentTypes.TextPlain);
+            obj.BinIndex("twitter").Set("jsmith123");
+            obj.BinIndex("email").Set("jsmith@basho.com");
+            CheckResult(client.Put(obj));
+        }
+
+        private IEnumerable<RiakObjectId> PutObjectsWithIndexValues()
         {
             var ids = new List<RiakObjectId>();
 
@@ -105,30 +249,28 @@ namespace RiakClientExamples.Dev.Using
             ids.Add(veronicaId);
             CheckResult(client.Put(veronica));
 
-            var idxId = new RiakIndexId("indexes", "people", "field1");
-            var rslt = client.GetSecondaryIndex(idxId, "val1", "val4");
-            CheckResult(rslt);
+            return ids;
+        }
 
-            var idxRslt = rslt.Value;
-            foreach (var keyTerm in idxRslt.IndexKeyTerms)
+        private static void PrintKeys(RiakIndexResult rslt, bool printTerms = false)
+        {
+            foreach (var kt in rslt.IndexKeyTerms)
             {
-                Debug.WriteLine(keyTerm.Key);
-            }
-
-            foreach (var id in ids)
-            {
-                DeleteObject(id);
+                PrintKeyTerm(kt, printTerms);
             }
         }
 
-        private void PutJohnSmith()
+        private static void PrintKeyTerm(RiakIndexKeyTerm kt, bool printTerm = false)
         {
-            id = new RiakObjectId("default", "users", "john_smith");
-            var obj = new RiakObject(id, "...user data...",
-                RiakConstants.ContentTypes.TextPlain);
-            obj.BinIndex("twitter").Set("jsmith123");
-            obj.BinIndex("email").Set("jsmith@basho.com");
-            CheckResult(client.Put(obj));
+            if (printTerm)
+            {
+                var args = new[] { kt.Key, kt.Term };
+                Debug.WriteLine(format: "Key: {0} Term: {1}", args: args);
+            }
+            else
+            {
+                Debug.WriteLine(kt.Key);
+            }
         }
     }
 }
