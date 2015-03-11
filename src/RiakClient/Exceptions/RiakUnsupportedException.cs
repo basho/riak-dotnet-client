bb70faedf874692b8b66d864a5ab06a4974581d3
Alex Moore <moore.alex@gmail.com>
2015-03-11 10:55:14 -0400

    Docs for Exceptions namespace.
    Former-commit-id: 8fc99669cfc296cbfa37acd35c26631e57024a1e

--- src/RiakClient/Exceptions/RiakUnsupportedException.cs
+++ src/RiakClient/Exceptions/RiakUnsupportedException.cs
@@ -21,18 +21,33 @@ namespace RiakClient.Exceptions
 {
     using System;
 
+    /// <summary>
+    /// Represents an error that is raised when conflicting Riak features are used together.
+    /// </summary>
     [Serializable]
     public class RiakUnsupportedException : RiakException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RiakUnsupportedException"/> class.
+        /// </summary>
         public RiakUnsupportedException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RiakUnsupportedException"/> class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
         public RiakUnsupportedException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RiakUnsupportedException"/> class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public RiakUnsupportedException(string message, Exception innerException)
             : base(message, innerException)
         {
