878a69693a78f546072e798e56016d6b4c6f3aea
Luke Bakken <luke@bakken.io>
2015-03-13 09:13:26 -0700

    More update examples

--- src/RiakClientExamples/Dev/Using/Updates.cs
+++ src/RiakClientExamples/Dev/Using/Updates.cs
@@ -18,6 +18,7 @@
 
 namespace RiakClientExamples.Dev.Using
 {
+    using System.Text;
     using NUnit.Framework;
     using RiakClient;
     using RiakClient.Models;
@@ -28,15 +29,48 @@ namespace RiakClientExamples.Dev.Using
     public sealed class Updates : ExampleBase
     {
         [Test]
-        public void ReadingObjects()
+        public void ExampleUpdate()
         {
-            id = new RiakObjectId("animals", "dogs", "rufus");
-            Assert.AreEqual("animals", id.BucketType);
-            Assert.AreEqual("dogs", id.Bucket);
-            Assert.AreEqual("rufus", id.Key);
+            id = PutCoach("seahawks", "Pete Carroll");
+        }
 
-            rslt = client.Get(id);
-            Assert.AreEqual(ResultCode.NotFound, rslt.ResultCode);
+        [Test]
+        public void UpdateCoachExample()
+        {
+            id = PutCoach("packers", "Old Coach");
+            UpdateCoach("packers", "Vince Lombardi");
+
+            id = new RiakObjectId("siblings", "coaches", "packers");
+            var getResult = client.Get(id);
+
+
+            RiakObject packers = getResult.Value;
+            Assert.AreEqual("Vince Lombardi", Encoding.UTF8.GetString(packers.Value));
+            Assert.AreEqual(0, packers.Siblings.Count);
+
+            rslt = getResult;
+        }
+
+        private RiakObjectId PutCoach(string team, string coach)
+        {
+            var id = new RiakObjectId("siblings", "coaches", team);
+            var obj = new RiakObject(id, coach,
+                RiakConstants.ContentTypes.TextPlain);
+            var rslt = client.Put(obj);
+            CheckResult(rslt);
+
+            return id;
+        }
+
+        private void UpdateCoach(string team, string newCoach)
+        {
+            id = new RiakObjectId("siblings", "coaches", team);
+            var getResult = client.Get(id);
+            CheckResult(getResult);
+
+            RiakObject obj = getResult.Value;
+            obj.SetObject<string>(newCoach, RiakConstants.ContentTypes.TextPlain);
+            rslt = client.Put(obj);
         }
     }
 }
