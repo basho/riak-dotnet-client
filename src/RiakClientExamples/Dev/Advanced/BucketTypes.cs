fb22356448de73daf7013884b554d4aa201805c6
Luke Bakken <luke@bakken.io>
2015-03-13 10:52:04 -0700

    Advanced / Bucket Types is complete.
    Former-commit-id: bc5031ccd62c4ccaca46dc8cbd8af9cefc755152

---
+++ src/RiakClientExamples/Dev/Advanced/BucketTypes.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="BucketTypes.cs" company="Basho Technologies, Inc.">
+// Copyright (c) 2015 - Basho Technologies, Inc.
+//
+// This file is provided to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file
+// except in compliance with the License.  You may obtain
+// a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// </copyright>
+
+namespace RiakClientExamples.Dev.Using
+{
+    using NUnit.Framework;
+    using RiakClient;
+    using RiakClient.Models;
+
+    /*
+     * http://docs.basho.com/riak/latest/dev/advanced/bucket-types/
+     */
+    public sealed class BucketTypes : ExampleBase
+    {
+        [Test]
+        public void UsingDefaultBucket()
+        {
+            var id = new RiakObjectId("my_bucket", "my_key");
+            var rslt = client.Get(id);
+        }
+
+        [Test]
+        public void UsingBucketType()
+        {
+            var id1 = new RiakObjectId("type1", "my_bucket", "my_key");
+            var id2 = new RiakObjectId("type2", "my_bucket", "my_key");
+            var rslt1 = client.Get(id1);
+            var rslt2 = client.Get(id2);
+        }
+
+        [Test]
+        public void DefaultBucketCanBeSpecifiedAsType()
+        {
+            id = new RiakObjectId("default", "my_bucket", "my_key");
+            var obj1 = new RiakObject(id, "value", RiakConstants.ContentTypes.TextPlain);
+            var putRslt = client.Put(obj1);
+            CheckResult(putRslt);
+
+            var id2 = new RiakObjectId("my_bucket", "my_key");
+            var getRslt = client.Get(id2);
+            CheckResult(getRslt);
+
+            RiakObject obj2 = getRslt.Value;
+            Assert.AreEqual(obj1.Value, obj2.Value);
+        }
+
+        [Test]
+        public void CreatingBucketInSpecificBucketType()
+        {
+            id = new RiakObjectId("no_siblings", "sensitive_user_data", "user19735");
+            var obj = new RiakObject(id, "{\"name\":\"Bob\"}");
+            rslt = client.Put(obj);
+        }
+
+        [Test]
+        public void CreatingBucketInSpecificBucketTypeExampleTwo()
+        {
+            id = new RiakObjectId("no_siblings", "old_memes", "all_your_base");
+            var obj = new RiakObject(id, "all your base are belong to us",
+                RiakConstants.ContentTypes.TextPlain);
+            rslt = client.Put(obj);
+        }
+    }
+}
